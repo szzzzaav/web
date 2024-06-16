5948b006cecb9ccdc13140adea4ac5a03f2e6b8f
szzzzaav <sulaimuzzz@sina.com>
2024-06-16 12:40:58 +0800

    css-loader&style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,12 @@ const path = require("path");
 module.exports = {
   devtool: false,
   mode: "none",
-  entry: "./src/index.js",
+  entry: ["./styles/bg.css", "./src/insertbg.js"],
   output: {
     filename: "bundle.js",
     path: path.join(__dirname, "output"),
+  },
+  module: {
+    rules: [{ test: /.css$/, use: ["style-loader", "css-loader"] }],
   },
 };
