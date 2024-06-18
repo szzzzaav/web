59137c134aff83091d6a12d1cfdff29343034ff9
szzzzaav <sulaimuzzz@sina.com>
2024-06-18 10:15:02 +0800

    watch-pattern

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-import "./insertbg";
-import bg from "../imgs/bg.jpg";
-
-const img = new Image();
-img.src = bg;
-document.body.append(img);
+// import "./insertbg";
+// import bg from "../imgs/bg.jpg";
+const f = () => {
+  console.log(123);
+};
+f();
+// const img = new Image();
+// img.src = bg;
+// document.body.append(img);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   devtool: false,
@@ -7,11 +9,32 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.join(__dirname, "output"),
+    // 网站的根目录
+    publicPath: "output/",
   },
   module: {
     rules: [
       { test: /\.css$/, use: ["style-loader", "css-loader"] },
-      { test: /\.jpg$/, use: ["file-loader"] },
+      {
+        test: /\.jpg$/,
+        use: { loader: "url-loader", options: { limit: 10 * 1024 } },
+      },
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: { presets: ["@babel/preset-env"] },
+        },
+      },
     ],
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      meta: {
+        viewport: "width=device-width",
+      },
+      filename: "index.html",
+    }),
+  ],
 };
